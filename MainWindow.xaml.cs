6f70b0364bd627d3a2aeb32907589f94cb2f9cbb
M4zek <mazur13aa@gmail.com>
2022-08-07 23:23:48 +0200

    - Create a class that stores settings - Creating handlers for the gui and updating the MyProperties class with them

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,28 +1,99 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace BandwidthMonitoring
 {
-    /// <summary>
-    /// Logika interakcji dla klasy MainWindow.xaml
-    /// </summary>
+
+
+    
     public partial class MainWindow : Window
     {
+
+        private MyProperties myProperties;
+
+
         public MainWindow()
         {
             InitializeComponent();
+            InitWindowButton();
+            InitStartButton();
+            InitClasses();
+            InitComboBoxs();
+        }
+
+        private void InitClasses()
+        {
+            myProperties = new MyProperties();
+        }
+
+
+        // Initialize combo box with values from network interfece
+        // and set boxes to default values
+        private void InitComboBoxs()
+        {
+            PerformanceCounterCategory pcNetworkInterface = new PerformanceCounterCategory("Network Interface");
+            String[] cardName = pcNetworkInterface.GetInstanceNames();
+            if (cardName.Length > 0)
+            {
+                foreach (string card in cardName)
+                {
+                    comboBox_NetworkCard.Items.Add(card);
+                }
+                comboBox_NetworkCard.SelectedIndex = 0;
+            }
+            comboBox_Speed.SelectedIndex = 0;
+            comboBox_TimeToShutDown.SelectedIndex = 0;
+        }
+
+
+        // Initializa window button to minimaized window and exit application
+        private void InitWindowButton()
+        {
+            MinimizeButton.Click += (s, e) => WindowState = WindowState.Minimized;
+            MaximizeButton.Click += (s, e) => WindowState = WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized;
+            CloseButton.Click += (s, e) => Close();
+        }
+
+        // Init start button to test 
+        private void InitStartButton()
+        {
+            StartButton.Click += (s, e) =>
+            {
+                Console.WriteLine(myProperties.getParameters());
+            };
+        }
+
+        // Listening for bandwidth changes in the combo box 
+        private void comboBox_Speed_change(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBoxItem item = (ComboBoxItem) comboBox_Speed.SelectedItem;
+            string sItem = item.Content.ToString();
+            if (sItem.Equals("Do not turn off")) myProperties.downloadSpeed = -1;
+            else myProperties.downloadSpeed = double.Parse(sItem.Split(' ')[0]);
+        }
+
+        // Listening for time to close changes in the combo box 
+        private void comboBox_Time_change(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBoxItem item = (ComboBoxItem) comboBox_TimeToShutDown.SelectedItem;
+            string sItem = item.Content.ToString();
+            if (sItem.Equals("Immediately")) myProperties.timeToShutDown = -1;
+            else myProperties.timeToShutDown = double.Parse(sItem.Split(' ')[0]);
+        }
+
+        // Listening for save to file changes in the check box
+        private void Save_Checked(object sender, RoutedEventArgs e)
+        {
+            myProperties.saveToFile = (bool)comboBox_SaveToFile.IsChecked;
+        }
+
+        // Listening for changes in the check box
+        private void StayAwake(object sender, RoutedEventArgs e)
+        {
+            // TODO Create a class that implements the process
+            // of keeping a computer in a ready state 
         }
     }
 }
---
+++ class/Properties.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BandwidthMonitoring
+{
+    public class MyProperties
+    {
+        public String tmpName { get; set; }
+        public bool saveToFile { get; set; }
+        public double downloadSpeed { get; set; }
+        public double timeToShutDown { get; set; }
+
+        public MyProperties()
+        {
+            this.downloadSpeed = 0;
+            this.timeToShutDown = 0;
+            this.saveToFile = false;
+            this.tmpName = "";
+        }
+
+        public String getParameters()
+        {
+            return $"MyProperties \nTMPNAME: {tmpName}\nSave? {saveToFile}\nDownload {downloadSpeed}\nTime {timeToShutDown}";
+        }
+    }
+}
